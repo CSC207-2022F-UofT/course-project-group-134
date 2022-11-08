61505556cb939bc5599511e99e199b16cc6ef679
lookcook <aditya.bandekar@mail.utoronto.ca>
2022-11-08 11:30:53 -0500

    Implemented Order

--- src/main/java/entities/Order.java
+++ src/main/java/entities/Order.java
@@ -1,4 +1,14 @@
 package entities;
 
 public class Order {
+
+    DiningHall diningHall;
+    Buyer buyer;
+    FoodItem foodItem;
+
+    public Order(DiningHall diningHall, Buyer buyer, FoodItem foodItem) {
+        this.diningHall = diningHall;
+        this.buyer = buyer;
+        this.foodItem = foodItem;
+    }
 }
