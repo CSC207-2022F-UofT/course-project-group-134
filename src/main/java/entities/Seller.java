f666d71f842cd05bdc3041fe26e7eb5a0f2da170
VivianLiu2357 <vivianliu2357@gmail.com>
2022-11-08 12:04:44 -0500

    Adding temporary changes to the User class and its subclasses.

--- src/main/java/entities/Seller.java
+++ src/main/java/entities/Seller.java
@@ -1,14 +1,23 @@
 package entities;
 
+import java.util.ArrayList;
+
 public class Seller extends User implements Reviewable{
 
     private MealPlan mealPlan;
+    private ArrayList<Review> reviews;
+    double starAverage;
     public Seller(String name, String password, MealPlan mealPlan){
         super(name, password);
         this.mealPlan = mealPlan;
     }
 
     public void fulfillOrder(Order order){
+        order.setSeller(this);
+    }
+
+    public void getReview(Review review){
+        this.reviews.add(review);
     }
 
 
