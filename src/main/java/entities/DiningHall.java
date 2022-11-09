2e7fd4510f82cf2f2fc3c44b636f1cd547254c9f
benliu <benliu0001@gmail.com>
2022-11-09 12:52:15 -0500

    Made Reviewable interface and added get average review score to dining hall class, removed final from instance attributes of dining hall class

--- src/main/java/entities/DiningHall.java
+++ src/main/java/entities/DiningHall.java
@@ -7,10 +7,10 @@ import java.util.ArrayList;
 
 public class DiningHall implements Reviewable {
 
-    private final String location;
-    private final String name;
-    private final Menu menu;
-    private final ArrayList<Review> reviews;
+    private String location;
+    private String name;
+    private Menu menu;
+    private ArrayList<Review> reviews;
     double starAverage;
 
     public DiningHall(String location, String name, Menu menu) {
