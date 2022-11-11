b71ef35bfa5bc13a9bacd02d01540e54bed09f5a
deonchan2012 <deonchan2012@gmail.com>
2022-11-11 18:59:54 -0500

    removed placeOrder from Buyer entity

--- src/main/java/entities/Buyer.java
+++ src/main/java/entities/Buyer.java
@@ -4,8 +4,4 @@ public class Buyer extends User{
     public Buyer(String name, String password, String email){
         super(name, password, email);
     }
-
-    public void placeOrder(Order order){
-        // TODO place in database
-    }
 }
