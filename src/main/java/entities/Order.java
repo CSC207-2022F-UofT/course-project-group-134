e30725deafd891665ee30de3a28065ce6a328b63
deonchan2012 <deonchan2012@gmail.com>
2022-11-11 19:02:38 -0500

    added 3 date attributes that store creation, acceptance, and completion

--- src/main/java/entities/Order.java
+++ src/main/java/entities/Order.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Order {
@@ -8,16 +9,26 @@ public class Order {
     private Buyer buyer;
     private ArrayList<FoodItem> foodItemList;
     private Seller seller;
+    private LocalDateTime creationTime;
+    private LocalDateTime acceptTime;
+    private LocalDateTime completionTime;
 
-    public Order(DiningHall diningHall, Buyer buyer, ArrayList<FoodItem> foodItemList) {
+    public Order(DiningHall diningHall, Buyer buyer, ArrayList<FoodItem> foodItemList, LocalDateTime creationTime) {
         this.diningHall = diningHall;
         this.buyer = buyer;
         this.foodItemList = foodItemList;
+        this.creationTime = creationTime;
     }
 
     public void setSeller(Seller seller) {
         this.seller = seller;
     }
+
+    public void setAcceptTime(LocalDateTime acceptTime) {this.creationTime = creationTime;}
+
+    public void setAcceptTime(LocalDateTime acceptTime) {this.creationTime = creationTime;}
+
+    public void setCompletionTime(LocalDateTime completionTime) {this.completionTime = completionTime;}
 
     public Seller getSeller() {
         return seller;
@@ -34,4 +45,5 @@ public class Order {
     public ArrayList<FoodItem> getFoodItemList() {
         return foodItemList;
     }
+
 }
