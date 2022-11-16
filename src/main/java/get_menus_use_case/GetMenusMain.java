e0a2343dda826261f79d87147ebf48b312f7707a
deonchan2012 <deonchan2012@gmail.com>
2022-11-16 04:30:09 -0500

    Added order access properly and renamed file from typo

---
+++ src/main/java/get_menus_use_case/GetMenusMain.java
@@ -0,0 +1,31 @@
+package get_menus_use_case;
+
+import entities.MenuFactory;
+import entities.OrderFactory;
+import order_use_case.*;
+import java.io.IOException;
+
+public class GetMenusMain {
+
+    public static void main(String[] args) throws Exception {
+        GetMenusOutputBoundary getMenusPresenter = new GetMenusPresenter();
+        MenuFactory menuFactory = new MenuFactory();
+        GetMenusInputBoundary getMenusInteractor = new GetMenusInteractor(getMenusPresenter, menuFactory);
+        GetMenusController getMenusController = new GetMenusController(getMenusInteractor);
+
+        OrderDsGateway orders;
+        try {
+            orders = new FileOrder("./orders.csv");
+        } catch (IOException e) {
+            throw new RuntimeException("Could not create file.");
+        }
+        // TODO: OrderFactory is ENTIRELY UNUSED because it is completely unnecessary right now
+        // Maybe we should add a method that calculates the price for us?
+        OrderFactory orderFactory = new OrderFactory();
+        OrderOutputBoundary orderPresenter = new OrderPresenter();
+        OrderInputBoundary orderInteractor = new OrderInteractor(orders, orderPresenter, orderFactory);
+        OrderController orderController = new OrderController(orderInteractor);
+
+        OrderView orderView = new OrderView(orderController, getMenusController);
+    }
+}
