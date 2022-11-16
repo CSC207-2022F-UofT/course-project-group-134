42839a0a07da77a71b7c94bb842a5307eaa60853
deonchan2012 <deonchan2012@gmail.com>
2022-11-16 04:28:19 -0500

    Deleted seller name and email

--- src/main/java/order_use_case/OrderRequestModel.java
+++ src/main/java/order_use_case/OrderRequestModel.java
@@ -4,17 +4,13 @@ public class OrderRequestModel {
 
     public final String buyerName;
     private final String buyerEmail;
-    private final String sellerName;
-    private final String sellerEmail;
 
     private final String residence;
     private final String[] foodItems;
 
-    public OrderRequestModel(String buyerName, String buyerEmail, String sellerName, String sellerEmail, String residence, String[] foodItems) {
+    public OrderRequestModel(String buyerName, String buyerEmail, String residence, String[] foodItems) {
         this.buyerName = buyerName;
         this.buyerEmail = buyerEmail;
-        this.sellerName = sellerName;
-        this.sellerEmail = sellerEmail;
         this.residence = residence;
         this.foodItems = foodItems;
     }
@@ -25,14 +21,6 @@ public class OrderRequestModel {
 
     public String getBuyerEmail() {
         return buyerEmail;
-    }
-
-    public String getSellerName() {
-        return sellerName;
-    }
-
-    public String getSellerEmail() {
-        return sellerEmail;
     }
 
     public String getResidence() {
