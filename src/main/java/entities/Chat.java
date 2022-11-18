d4a189fcaea3ca047177bdd80ebbb0447f275e5b
aaron <aa.ma@mail.utoronto.ca>
2022-11-18 13:03:36 -0500

    Created entities for chat functionality

--- src/main/java/entities/Chat.java
+++ src/main/java/entities/Chat.java
@@ -3,26 +3,25 @@ package entities;
 import java.util.ArrayList;
 
 public class Chat {
+    private ArrayList<ChatMessage> chatLog;
     private User user1, user2;
-    private ArrayList<String> chatLog;
+    public Chat() {
+        this.chatLog = new ArrayList<>();
+    }
 
-    public Chat(User user1, User user2) {
+    public Chat(User user1, User user2){
         this.chatLog = new ArrayList<>();
         this.user1 = user1;
         this.user2 = user2;
     }
 
-    public ArrayList<String> getChatLog() {
+    public ArrayList<ChatMessage> getChatLog() {
         return chatLog;
     }
-
-    public User getUser1() {
-        return user1;
+    public void sendMessage(ChatMessage message){
+        chatLog.add(message);
     }
 
-    public User getUser2() {
-        return user2;
-    }
 
-    // TODO a lot of other stuff...
+
 }
